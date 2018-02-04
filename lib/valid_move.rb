2dcd5b486e0bd27229d2f839810aaef80c999920
Andrew Trent <andrewtrentdesign@gmail.com>
2018-02-04 11:33:39 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -5,17 +5,28 @@
 
 
 def valid_move?(board,index)
-  def position_taken?  
+  
+    # check if the specified position is on the board
+  def move_on_board?(board, index)
+    if index >= 0 && index <= 8
+      true 
+    else
+      false
+    end
+  end 
+  
+  # check if the specified position is taken
+  def position_taken?(board, index) 
     if board[index] == nil
         return false
     if board[index].strip == ""
-        true
-    else
         false
+    else
+        true
     end
-  end  
-  if index >= 0 && index <= 8
-    true 
+  end 
+  if move_on_board?(board, index) && !position_taken(board, index)
+    true
   else
     false
   end
